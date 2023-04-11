9123595c15c43e56ce1018d2d56874a832a40302
Vedant Gandhi <vedantgandhi24@gmail.com>
2023-04-11 02:09:01 -0700

    added build files

--- index.js
+++ index.js
@@ -3,9 +3,13 @@ const connectDB = require("./config/db");
 var cors = require("cors");
 const dotenv = require("dotenv");
 dotenv.config();
+const path = require("path");
 
 const app = express();
 
+const PORT = process.env.PORT || 8000;
+
+app.use(express.static(path.join(__dirname, "./build")));
 app.use(cors());
 
 //Connect Database
@@ -21,6 +25,9 @@ app.get("/test", (req, res) => {
 	res.send("WELCOME!!!");
 });
 
-const PORT = process.env.PORT || 8000;
+app.get("/*", (req, res) => {
+	// console.log(path.join(__dirname, "build", "index.html"));
+	res.sendFile(path.join(__dirname, "build", "index.html"));
+});
 
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
